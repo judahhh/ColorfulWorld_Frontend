87bf001d68d3433dffc72c21c102b32cf9a64945
judahhh <jdh99080516@gmail.com>
2023-04-02 18:19:49 +0900

    feat : 메인 페이지 소개용 카드 슬라이드 구현

--- my-app/src/component/IntroduceCard.js
+++ my-app/src/component/IntroduceCard.js
@@ -1,51 +1,82 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import Ishihara1 from "./img/Ishihara_1.png";
+import Ishihara2 from "./img/Ishihara_2.png";
 
-const Container = styled.div`
-  width: 500px;
+const StyleIntroduceCard = styled.main`
+  width: 480px;
+  height: 480px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.05);
+  border-radius: 10px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: auto;
-  height: 1000px;
-  overflow: hidden;
+  margin-top: 30px;
 `;
 
-const Box = styled.div`
-  height: 350px;
-  width: 350px;
-  border-radius: 7px;
-  display: flex;
-  background-color: whitesmoke;
-  display: flex;
-  transition: all 1s ease;
-  margin-top: 50px;
-  margin-left: auto;
-  margin-right: auto;
-  box-shadow: 10px 5px 5px lightgray;
-  position: relative;
-  justify-content: center;
-  align-items: center;
-  /* position: absolute;
-  left: 50%;
-  top: 50%;
-  margin-left: -150px;
-  margin-top: -150px; */
+const StyleIntroduceTitle = styled.p`
+  font-size: 28px;
+  font-family: "Noto Serif KR", serif;
+  color: #f0dd22;
 `;
 
-const CardText = styled.p`
+const StyleImg = styled.img`
+  width: 260px;
+  height: 260px;
+`;
+const StyleCardText = styled.p`
   align-items: center;
-  color: darkgray;
+  color: black;
   font-family: "Noto Sans KR", sans-serif;
+  padding: 0px;
+  margin-bottom: 0px;
 `;
-
-const Img = styled.div``;
+const StyleBtnContainer = styled.div`
+  text-align: center;
+  margin: 20px;
+`;
+const StyleSlideBtn = styled.button`
+  width: 10px;
+  height: 12px;
+  border-radius: 50%;
+  background-color: darkgray;
+  border: none;
+  display: inline-block;
+  margin: 5px;
+  :hover {
+    cursor: pointer;
+  }
+`;
 
 const IntroduceCard = () => {
+  let [btnState, setBtnState] = useState(1);
   return (
     <>
-      <Container>
-        <Box>
-          <CardText>Introduce Page</CardText>
-        </Box>
-      </Container>
+      <StyleIntroduceCard>
+        <StyleIntroduceTitle>Welcome!</StyleIntroduceTitle>
+        {btnState == 1 ? (
+          <StyleImg src={Ishihara1}></StyleImg>
+        ) : (
+          <StyleImg src={Ishihara2}></StyleImg>
+        )}
+        {btnState == 1 ? (
+          <StyleCardText>
+            색각이상자 분들에게 보다 다채로운 세상을 선물하겠습니다.
+          </StyleCardText>
+        ) : (
+          <StyleCardText>
+            일반인과 색각이상자 모두가 이질감 없이 함께 볼 수 있는 서비스!
+          </StyleCardText>
+        )}
+
+        <StyleBtnContainer>
+          <StyleSlideBtn onClick={() => setBtnState(1)}></StyleSlideBtn>
+          <StyleSlideBtn onClick={() => setBtnState(2)}></StyleSlideBtn>
+        </StyleBtnContainer>
+      </StyleIntroduceCard>
     </>
   );
 };
