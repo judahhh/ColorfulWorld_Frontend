4c756638c0d62ed2a71cb1952003e5d09894cc29
judahhh <jdh99080516@gmail.com>
2023-04-08 04:16:50 +0900

    feat : 이메일 인증코드 입력 폼 추가 및 회원가입 시 데이터 서버로 전송

--- my-app/src/component/join/JoinForm.js
+++ my-app/src/component/join/JoinForm.js
@@ -1,33 +1,77 @@
 import styled from "styled-components";
 import StyleInput from "../commons/Input";
 import FormBtn from "../commons/FormBtn";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const StyleLoginForm = styled.form`
-display:flex;
-flex-direction:column;
-align-items:center;
-justify-content:space-between;
-height:400px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 450px;
+`;
 
+const JoinForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
+  const navigate = useNavigate();
 
-const JoinForm = () =>{
-    const onSubmit = (event) =>{//회원가입 제출했을시 함수
-        event.preventDefault()
-        const [email,password,password2] = event.target;
-        console.log(email.value,password.value,password2.value)
-    }
-    return (
-        <StyleLoginForm onSubmit={onSubmit}>
-            <div>
-            <StyleInput label="이메일" placeholder="example@coloful.world" type="text"/>
-            <StyleInput label="비밀번호" placeholder="********" type="password"/>
-            <StyleInput label="비밀번호확인" placeholder="********" type="password"/>
-            </div>
-            <FormBtn value="JOIN"/>
-        </StyleLoginForm>
-    )
-}
+  const onSubmit = async (e) => {
+    //회원가입 제출했을시 함수
+    e.preventDefault();
+    let body = {
+      email: email,
+      password: password,
+    };
+    await axios
+      .post("/register", body)
+      .then((response) => {
+        if (response.status === 200) {
+          alert("회원가입에 성공하셨습니다. 로그인을 진행해주세요! ");
+          navigate("/login");
+        } else {
+          alert("이미 회원가입 완료한 이메일입니다.");
+        }
+      })
+      .catch((err) => console.log("에러 : ", err));
+  };
+  return (
+    <StyleLoginForm onSubmit={onSubmit}>
+      <div>
+        <StyleInput
+          label="이메일"
+          placeholder="example@coloful.world"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <StyleInput
+          label="이메일 인증코드"
+          placeholder="이메일 인증 코드를 입력하세요"
+          type="text"
+        />
+        <StyleInput
+          label="비밀번호"
+          placeholder="********"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <StyleInput
+          label="비밀번호확인"
+          placeholder="********"
+          type="password"
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+        />
+      </div>
+      <FormBtn value="JOIN" />
+    </StyleLoginForm>
+  );
+};
 
 export default JoinForm;
