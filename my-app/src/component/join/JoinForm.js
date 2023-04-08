bbd4bf69595b97172cea9bebc2b1059919ce28e3
judahhh <jdh99080516@gmail.com>
2023-04-08 14:37:24 +0900

    feat : 회원가입 페이지 유효성 검사 기능 추가

--- my-app/src/component/join/JoinForm.js
+++ my-app/src/component/join/JoinForm.js
@@ -17,58 +17,74 @@ const JoinForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
+  const checkEmail = (e) => {
+    if (email !== "" && !email.includes("@") && !email.includes("."))
+      alert("올바르지 않은 이메일 형식입니다.");
+  };
 
   const navigate = useNavigate();
 
+  //회원가입 제출했을시 함수
   const onSubmit = async (e) => {
-    //회원가입 제출했을시 함수
     e.preventDefault();
-    let body = {
-      email: email,
-      password: password,
-    };
-    await axios
-      .post("/register", body)
-      .then((response) => {
-        if (response.status === 200) {
-          alert("회원가입에 성공하셨습니다. 로그인을 진행해주세요! ");
-          navigate("/login");
-        } else {
-          alert("이미 회원가입 완료한 이메일입니다.");
-        }
-      })
-      .catch((err) => console.log("에러 : ", err));
+
+    if (email === "") alert("이메일을 입력해주세요.");
+    else if (password === "") alert("비밀번호를 입력해주세요.");
+    else if (passwordConfirm === "") alert("비밀번호 확인을 입력해주세요");
+    else if (password !== passwordConfirm)
+      alert("비밀번호와 비밀번호 확인이 같지 않습니다.\n다시 입력해주세요.");
+    else if (checkEmail(email) === false)
+      alert("이메일 형식을 다시 확인해주세요.");
+    else if (password !== "" && !(password.length < 8))
+      alert(
+        "비밀번호는 영문자, 숫자, 특수문자를 포함한 8자리 이상이어야 합니다."
+      );
+    else {
+      await axios
+        .post("/register", { email, password })
+        .then((response) => {
+          console.log("서버에 전송");
+          if (response.status === 200) {
+            alert("회원가입에 성공하셨습니다. 로그인을 진행해주세요! ");
+            navigate("/login");
+          } else {
+            alert("이미 회원가입 완료한 이메일입니다.");
+          }
+        })
+        .catch((error) => console.error("에러 : ", error));
+    }
   };
+
   return (
     <StyleLoginForm onSubmit={onSubmit}>
-      <div>
-        <StyleInput
-          label="이메일"
-          placeholder="example@coloful.world"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <StyleInput
-          label="이메일 인증코드"
-          placeholder="이메일 인증 코드를 입력하세요"
-          type="text"
-        />
-        <StyleInput
-          label="비밀번호"
-          placeholder="********"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <StyleInput
-          label="비밀번호확인"
-          placeholder="********"
-          type="password"
-          value={passwordConfirm}
-          onChange={(e) => setPasswordConfirm(e.target.value)}
-        />
-      </div>
+      <StyleInput
+        label="이메일"
+        placeholder="example@coloful.world"
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        onBlur={checkEmail}
+      />
+      <StyleInput
+        label="이메일 인증코드"
+        placeholder="이메일 인증 코드를 입력하세요"
+        type="text"
+      />
+      <StyleInput
+        label="비밀번호"
+        placeholder="********"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <StyleInput
+        label="비밀번호확인"
+        placeholder="********"
+        type="password"
+        value={passwordConfirm}
+        onChange={(e) => setPasswordConfirm(e.target.value)}
+      />
+
       <FormBtn value="JOIN" />
     </StyleLoginForm>
   );
