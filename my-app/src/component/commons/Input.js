e49270e44af9e687a6397369bea74dd078b0acd1
judahhh <jdh99080516@gmail.com>
2023-04-08 14:36:34 +0900

    feat : Input 컴포넌트에 props 추가하여 value 값 연결

--- my-app/src/component/commons/Input.js
+++ my-app/src/component/commons/Input.js
@@ -1,40 +1,46 @@
 import styled from "styled-components";
 
 const StyleInputBlock = styled.div`
-    display:flex;
-    flex-direction:column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 const StyleInput = styled.input`
-    width:320px;
-    height:44px;
-    background-color:#FCFCFC;
-    border: 1px solid #dee2e6;
-    border-radius: 4px;
-    padding:0 12px;
-    font-size:16px;
-    font-family: 'Noto Serif KR', serif;
-    margin-bottom:20px;
-    &:focus{
-        outline:none;
-        border-color:#F0DD22;
-    }
-`
+  width: 320px;
+  height: 44px;
+  background-color: #fcfcfc;
+  border: 1px solid #dee2e6;
+  border-radius: 4px;
+  padding: 0 12px;
+  font-size: 16px;
+  font-family: "Noto Serif KR", serif;
+  margin-bottom: 20px;
+  &:focus {
+    outline: none;
+    border-color: #f0dd22;
+  }
+`;
 
 const StyleLabel = styled.label`
-    font-size:12px;
-    margin-bottom:12px;
-    font-family: 'Noto Sans KR', sans-serif;
-`
+  font-size: 12px;
+  margin-bottom: 12px;
+  font-family: "Noto Sans KR", sans-serif;
+`;
 
-const Input = (props) =>{
-    let {label,placeholder,type} = props
-    return(
-        <StyleInputBlock>
-            <StyleLabel>{label}</StyleLabel>
-            <StyleInput placeholder={placeholder} type={type}/>
-        </StyleInputBlock>
-    )
-}
+const Input = (props) => {
+  let { label, placeholder, type, value, onChange, onBlur } = props;
+  return (
+    <StyleInputBlock>
+      <StyleLabel>{label}</StyleLabel>
+      <StyleInput
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+    </StyleInputBlock>
+  );
+};
 
 export default Input;
