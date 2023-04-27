3f202c214a7ed8d850bba1bca3ec36ce686d35d5
judahhh <jdh99080516@gmail.com>
2023-04-28 03:14:00 +0900

    feat : 로그인 안 했을 시 로그인 페이지로 이동하는 라우팅 추가

--- my-app/src/page/MainPage.js
+++ my-app/src/page/MainPage.js
@@ -3,6 +3,7 @@ import IntroduceCard from "../component/main/IntroduceCard";
 import FormBtn from "../component/commons/FormBtn";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const StyleMain = styled.div`
   display: flex;
@@ -21,7 +22,15 @@ const StyleBtnContainer = styled.div`
 const MainPage = () => {
   //FormBtn은 Component이기 때문에 onClick이 되지 않아서 props로 넘겼습니다!!
   const navigate = useNavigate();
-  //로그인 확인할 때 사용하면 사용자를 구분하면 좋을거 같아요!
+
+  //페이지 렌더링 시에 로그인이 안 되어 있을 경우 로그인 페이지로 이동
+  useEffect(() => {
+    //로그인 확인할 때 사용하면 사용자를 구분하면 좋을거 같아요!
+    if (localStorage.getItem("loginState") !== true) {
+      navigate("/login");
+    }
+  }, []);
+
   const gotochange = () => {
     navigate("/change");
   };
