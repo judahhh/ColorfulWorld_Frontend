e03427de16e6a3c484a51ecbc25502206ed3389e
judahhh <jdh99080516@gmail.com>
2023-05-17 03:54:40 +0900

    style : Not Found 페이지 스타일 수정

--- my-app/src/page/NotFoundPage.js
+++ my-app/src/page/NotFoundPage.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+const Style404Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const Style404Card = styled.div`
   width: 480px;
   height: 480px;
@@ -10,27 +16,44 @@ const Style404Card = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
+  justify-content: center;
+  margin: 30px;
 `;
 const Style404Text = styled.div`
   font-family: "Noto Sans KR", sans-serif;
+  font-size: 25px;
+  margin: 10px;
+  text-align: center;
 `;
 
 const StyleGoHomeBtn = styled.button`
-  width: 100px;
+  width: 200px;
   height: 40px;
-  border: solid 1px #ebd500;
+  border: solid 2px #ebd500;
   font-family: "Noto Sans KR", sans-serif;
   background-color: white;
   color: #ebd500;
+  font-size: 20px;
   border-radius: 5px;
+  margin: 30px;
+  :hover {
+    cursor: pointer;
+  }
 `;
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
   return (
-    <Style404Card>
-      <Style404Text>존재하지 않는 페이지입니다.</Style404Text>
-      <StyleGoHomeBtn>메인 홈으로 이동</StyleGoHomeBtn>
-    </Style404Card>
+    <Style404Wrapper>
+      <Style404Card>
+        <Style404Text>Not Found!</Style404Text>
+        <Style404Text>존재하지 않는 페이지입니다.</Style404Text>
+
+        <StyleGoHomeBtn onClick={() => navigate("/")}>
+          메인 홈으로 이동
+        </StyleGoHomeBtn>
+      </Style404Card>
+    </Style404Wrapper>
   );
 };
 
