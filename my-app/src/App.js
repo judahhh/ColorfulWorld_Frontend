1896ac09ef88d4174af78f5c3c9a03961270a1d3
judahhh <jdh99080516@gmail.com>
2023-05-30 01:51:57 +0900

    feat : Private Route 이용하여 로그인 여부 확인 후 분기 처리

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,17 +5,24 @@ import JoinPage from "./page/JoinPage"; //회원가입 페이지
 import ChangePage from "./page/ChangePage"; //이미지 업로드 페이지
 import ResultPage from "./page/ResultPage"; //결과 페이지
 import NotFoundPage from "./page/NotFoundPage"; //존재하지 않는 페이지
+import PrivateRoute from "./component/auth/PrivateRoute"; //리다이렉팅 페이지
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/join" element={<JoinPage />} />
-        <Route path="/change" element={<ChangePage />} />
-        <Route path="/result/:address" element={<ResultPage />} />
+        <Route element={<PrivateRoute auth={true} />}>
+          <Route path="/" element={<MainPage />} />
+        </Route>
+        <Route element={<PrivateRoute auth={false} />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/join" element={<JoinPage />} />
+        </Route>
+        <Route element={<PrivateRoute auth={true} />}>
+          <Route path="/change" element={<ChangePage />} />
+          <Route path="/result/:address" element={<ResultPage />} />
+        </Route>
         <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </BrowserRouter>
